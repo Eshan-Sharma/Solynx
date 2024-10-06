482631d510404557575e3f63cb7e5bafeaaa8560
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-10-06 10:34:15 +0530

    read redis queue

---
+++ src/app/api/deploy/route.ts
@@ -0,0 +1,15 @@
+import { createClient, commandOptions } from "redis";
+const subscriber = createClient();
+subscriber.connect();
+
+async function main() {
+  while (1) {
+    const response = await subscriber.brPop(
+      commandOptions({ isolated: true }),
+      "build-queue",
+      0
+    );
+    console.log(`Response from redis: ${response}`);
+  }
+}
+main();
