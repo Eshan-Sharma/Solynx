2f46861b201f1c7a6f46b49843afb8c684120516
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-11-05 13:05:25 +0530

    add next/image

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    webpack: (config, { isServer }) => {
-      if (!isServer) {
-        config.resolve.fallback = {
-          ...config.resolve.fallback,
-          fs: false,
-          net: false,
-          tls: false,
-        };
-      }
-      return config;
-    },
-  }
-  
-  export default nextConfig
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname:
+          "bafybeibagjmvlztpgswjqxzwwj7fuoct3a4tbkdxjk3qllj7nsrtoaigna.ipfs.w3s.link",
+        pathname: "/**",
+      },
+    ],
+  },
+  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
+    return config;
+  },
+};
+
+export default nextConfig;
